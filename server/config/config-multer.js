3818b53403a6edd25b1a39bd203a2d166c5c9e16
RaresDrg <dragan_rares13@yahoo.com>
2024-09-19 02:36:41 +0300

    public
    public

--- server/config/config-multer.js
+++ server/config/config-multer.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public");
+    cb(null, "/public");
   },
   filename: (req, file, cb) => {
     const fileExtension = path.extname(file.originalname);
