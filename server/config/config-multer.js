c98365c1152ad7f500c32281dd80fa1383998301
RaresDrg <dragan_rares13@yahoo.com>
2024-09-19 02:01:00 +0300

    test 3
    test 3

--- server/config/config-multer.js
+++ server/config/config-multer.js
@@ -2,20 +2,20 @@ import multer from "multer";
 import path from "path";
 // import fs from "fs/promises";
 
-// const publicDir = path.join(process.cwd(), "public");
-// checkFolder(publicDir);
+const publicDir = path.join(process.cwd(), "public");
+checkFolder(publicDir);
 
-// async function checkFolder(folderPath) {
-//   try {
-//     await fs.access(folderPath);
-//   } catch (error) {
-//     await fs.mkdir(folderPath);
-//   }
-// }
+async function checkFolder(folderPath) {
+  try {
+    await fs.access(folderPath);
+  } catch (error) {
+    await fs.mkdir(folderPath);
+  }
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public");
+    cb(null, publicDir);
   },
   filename: (req, file, cb) => {
     const fileExtension = path.extname(file.originalname);
