96da6ab69dc87c467c997b4fa06f3edd4280681e
RaresDrg <dragan_rares13@yahoo.com>
2024-11-09 16:53:27 +0200

    test path
    test path

--- client/src/utils/getBg.js
+++ client/src/utils/getBg.js
@@ -1,12 +1,19 @@
+import a from "../assets/images/backgrounds/micro/bg-1_micro.png";
+import b from "../assets/images/backgrounds/desktop/bg-1_desktop.jpg";
+
 export function getBg(variant, name) {
   switch (variant) {
     case "micro":
+      return a;
       return `/assets/${name}_micro.png`;
     case "mobile":
+      return b;
       return `images/backgrounds/mobile/${name}_mobile.jpg`;
     case "tablet":
+      return b;
       return `images/backgrounds/tablet/${name}_tablet.jpg`;
     case "desktop":
+      return b;
       return `images/backgrounds/desktop/${name}_desktop.jpg`;
     default:
       return "none";
